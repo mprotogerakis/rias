c684498eb4539f138f9218c19e93059514717bfc
Michael Protogerakis <michael.protogerakis@proto-hess.com>
2024-04-24 12:40:32 +0200

    typing

---
+++ typing_example.py
@@ -0,0 +1,8 @@
+from typing import List
+
+def double_numbers(numbers: List[int]) -> List[int]:
+    doubled = [num * 2 for num in numbers]
+    return doubled
+
+result = double_numbers([1, 2, 3])
+print(result)
